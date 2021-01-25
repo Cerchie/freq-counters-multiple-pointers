09bf05845ed9dda95696cb64c8429b3f0c9e4a25
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-25 11:29:53 -0700

    adds solution for first three

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, targetAvg) {
+    let left =0;
+    let right = arr.length -1;
+    //our pointers
+    while (left < right){ //our loop
+        let avg = (arr[left] + arr[right])/2;
+        if (avg === targetAvg){
+            return true;
+        } else if (avg > targetAvg){
+            right--;
+        } else {left ++}
+    }
+    return false;
 }
+
+
